2b347fe78d5f71924cc0307dc2da772d0193ab49
realyurimednikov <mednikov.iurii@gmail.com>
2021-07-02 20:36:41 +0200

    Added row item layout. SharedViewModel. Implemented insert new item logic

--- app/src/main/java/me/mednikov/todomatic/fragments/add/AddFragment.kt
+++ app/src/main/java/me/mednikov/todomatic/fragments/add/AddFragment.kt
@@ -2,11 +2,22 @@ package me.mednikov.todomatic.fragments.add
 
 import android.os.Bundle
 import android.view.*
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import me.mednikov.todomatic.R
+import me.mednikov.todomatic.data.models.TodoEntity
+import me.mednikov.todomatic.viewmodels.SharedViewModel
+import me.mednikov.todomatic.viewmodels.TodoViewModel
 
 
 class AddFragment : Fragment() {
+
+    private val mTodoViewModel: TodoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +31,39 @@ class AddFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_add) {
+            insertTodoToDatabase()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertTodoToDatabase() {
+        val title = view?.findViewById<EditText>(R.id.et_title)?.text.toString()
+        val description = view?.findViewById<EditText>(R.id.et_description)?.text.toString()
+        val priority = view?.findViewById<Spinner>(R.id.sp_priority)?.selectedItem.toString()
+
+        val validation = mSharedViewModel.validate(title, description)
+        if (validation) {
+            val entity = TodoEntity(
+                id = 0,
+                title = title,
+                priority = mSharedViewModel.parsePriority(priority),
+                description = description
+            )
+
+            mTodoViewModel.insert(entity)
+
+            Toast.makeText(requireContext(), "Task was added", Toast.LENGTH_SHORT).show()
+
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "You missed some fields!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+
 
 }
---
+++ app/src/main/java/me/mednikov/todomatic/viewmodels/SharedViewModel.kt
@@ -0,0 +1,26 @@
+package me.mednikov.todomatic.viewmodels
+
+import android.app.Application
+import android.text.TextUtils
+import androidx.lifecycle.AndroidViewModel
+import me.mednikov.todomatic.data.models.Priority
+
+class SharedViewModel(application: Application): AndroidViewModel(application) {
+
+    fun parsePriority(priority: String): Priority {
+        return when (priority){
+            "High Priority" -> {
+                Priority.HIGH}
+            "Medium Priority" -> {
+                Priority.MEDIUM}
+            "Low Priority" -> {
+                Priority.LOW}
+            else -> {
+                Priority.LOW}
+        }
+    }
+
+    fun validate(title: String, description: String): Boolean {
+        return !TextUtils.isEmpty(title) && !TextUtils.isEmpty(description)
+    }
+}
