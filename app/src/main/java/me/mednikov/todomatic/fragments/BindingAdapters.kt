0f47b65435cd4b5f99f1900b59ae1314b5cb21b0
realyurimednikov <mednikov.iurii@gmail.com>
2021-07-03 12:16:49 +0200

    Section 4: Implemented data binding for ListAdapter

--- app/src/main/java/me/mednikov/todomatic/fragments/BindingAdapters.kt
+++ app/src/main/java/me/mednikov/todomatic/fragments/BindingAdapters.kt
@@ -2,12 +2,16 @@ package me.mednikov.todomatic.fragments
 
 import android.view.View
 import android.widget.Spinner
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import me.mednikov.todomatic.R
 import me.mednikov.todomatic.data.models.Priority
+import me.mednikov.todomatic.data.models.TodoEntity
+import me.mednikov.todomatic.fragments.list.ListFragmentDirections
 
 class BindingAdapters {
 
@@ -43,5 +47,23 @@ class BindingAdapters {
             }
         }
 
+        @BindingAdapter("android:setPriorityCardColor")
+        @JvmStatic
+        fun setPriorityCardColor(view: CardView, priority: Priority){
+            when (priority) {
+                Priority.HIGH -> {view.setCardBackgroundColor(view.context.getColor(R.color.priority_high))}
+                Priority.MEDIUM -> {view.setCardBackgroundColor(view.context.getColor(R.color.priority_medium))}
+                Priority.LOW -> {view.setCardBackgroundColor(view.context.getColor(R.color.priority_low))}
+            }
+        }
+
+        @BindingAdapter("android:sendDataToUpdateFragment")
+        @JvmStatic
+        fun sendDataToUpdateFragment(view: ConstraintLayout, data: TodoEntity){
+            view.setOnClickListener{
+                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(data)
+                view.findNavController().navigate(action)
+            }
+        }
     }
 }
--- app/src/main/java/me/mednikov/todomatic/fragments/list/ListAdapter.kt
+++ app/src/main/java/me/mednikov/todomatic/fragments/list/ListAdapter.kt
@@ -1,50 +1,37 @@
 package me.mednikov.todomatic.fragments.list
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.cardview.widget.CardView
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import me.mednikov.todomatic.R
-import me.mednikov.todomatic.data.models.Priority
 import me.mednikov.todomatic.data.models.TodoEntity
+import me.mednikov.todomatic.databinding.RowItemBinding
 
 class ListAdapter: RecyclerView.Adapter<ListAdapter.TodoItemViewHolder> (){
 
     private var items = emptyList<TodoEntity>()
 
-    class TodoItemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+    class TodoItemViewHolder(private val binding:RowItemBinding):RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(todoEntity: TodoEntity) {
+            binding.todoItem = todoEntity
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): TodoItemViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = RowItemBinding.inflate(inflater, parent, false)
+                return TodoItemViewHolder(binding)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoItemViewHolder {
-        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.row_item, parent, false)
-        return TodoItemViewHolder(inflater)
+         return TodoItemViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: TodoItemViewHolder, position: Int) {
-        val title = holder.itemView.findViewById<TextView>(R.id.row_title)
-        val description = holder.itemView.findViewById<TextView>(R.id.row_description)
-        val priority = holder.itemView.findViewById<CardView>(R.id.priority_indicator)
-        val background = holder.itemView.findViewById<ConstraintLayout>(R.id.rowBackground)
-
-        title.text = items[position].title
-        description.text = items[position].description
-
-        when(items[position].priority){
-            Priority.HIGH -> {priority.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.priority_high))}
-            Priority.MEDIUM -> {priority.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.priority_medium))}
-            Priority.LOW -> {priority.setCardBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.priority_low))}
-        }
-
-        background.setOnClickListener {
-            val action = ListFragmentDirections.actionListFragmentToUpdateFragment(items[position])
-            holder.itemView.findNavController().navigate(action)
-        }
+        holder.bind(items[position])
     }
 
     override fun getItemCount(): Int {
